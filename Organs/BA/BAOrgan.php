4b4226f1079eab4daf8e43253af1285a7618d3d4
andrew <andrew@pcd>
2024-01-22 03:45:07 +0000

    Revert "Dynamic properties deprecated"
    This reverts commit fafb34eae0d31470464d59eff422dfcfb2374f80.

--- Organs/BA/BAOrgan.php
+++ Organs/BA/BAOrgan.php
@@ -18,7 +18,6 @@ abstract class BAOrgan extends \Import\Organ {
     
     protected ?int $releaseCrossfadeLengthMs=0;
     protected string $root="";
-    protected array $positions=[];
 
     /*
      * Create Windchest Groups. 1 per position/division + 1 for all noise effects
